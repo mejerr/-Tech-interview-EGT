f15f2527259b804598a6055086820a31465d8599
mejerr <georgy_90@abv.bg>
2021-09-03 21:55:49 +0300

    Home page (mocked data) and responsive styles

--- casino-game/src/components/home/home.js
+++ casino-game/src/components/home/home.js
@@ -1,9 +1,31 @@
 import React, { memo } from 'react';
+import Button from '../../components/common/buttons/button';
 
 const Home = memo((props) => {
-    const {} = props;
+    const { currentGame } = props;
 
-    return <div className="home-container">Say hello</div>;
+    return (
+        <div className="home-container">
+            <div className="welcome-screen-wrapper">
+                <div className="welcome-title">Welcome to our games</div>
+                <div className="welcome-subtitle">Current game is: {currentGame}</div>
+                <div className="choose-game">
+                    <Button title={"Choose game"}/>
+                </div>
+                <div className="welcome-games-buttons">
+                <div className="choose-game">
+                    <Button title={"Choose game"}/>
+                </div>
+                <div className="play-game">
+                    <Button title={"Play game"}/>
+                </div>
+                </div>
+            </div>
+        </div>);
 });
+
+Home.defaultProps = {
+    currentGame: ''
+}
 
 export default Home;
