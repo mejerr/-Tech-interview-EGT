b75b8013c381c09b3cdf6a8cc15db3c96bd4a766
mejerr <georgy_90@abv.bg>
2021-09-05 01:13:30 +0300

    FinalCountdown game added

--- casino-game/src/components/games/final-countdown-game.js
+++ casino-game/src/components/games/final-countdown-game.js
@@ -1,23 +1,45 @@
-import React, { memo, useCallback } from 'react';
+import React, { memo, useCallback, useEffect } from 'react';
+import Slot from '../common/slot'
 
 const FinalCountdown = memo((props) => {
-    const { fields } = props;
+    const { slots, selectSlot, consumeSlots } = props;
 
-    const renderFields = useCallback((field) => {
-        return <div>123</div>
-    }, []);
+    const renderSlots = useCallback(({ index, isSelected }) => {
+        return (
+            <Slot
+                key={index}
+                selectSlot={selectSlot}
+                number={index}
+                isSelected={isSelected}
+            />
+        )
+    }, [selectSlot]);
+
+    useEffect(() => {
+        if (!slots.length) {
+            consumeSlots();
+        }
+    }, [consumeSlots, slots])
 
     return (
-        <div className="final-countdown-container">
-            <div className="main-square">
-                {fields.map(renderFields)}
+        <div className="final-countdown-container content">
+            <div className="final-countdown-wrapper">
+                <div className="slots-container">
+                    {slots.map(renderSlots)}
+                </div>
+                <div className="timer-field"></div>
+                <div className="total-field"></div>
+                <div className="draw-counts-field"></div>
+                <div className="bet-sum-field"></div>
             </div>
         </div>
     );
 });
 
 FinalCountdown.defaultProps = {
-    fields: []
+    slots: [],
+    selectSlot: () => {},
+    consumeSlots: () => {}
 }
 
 export default FinalCountdown;
--- casino-game/src/containers/games/final-countdown-game.js
+++ casino-game/src/containers/games/final-countdown-game.js
@@ -1,8 +1,11 @@
 import { connect } from 'react-redux';
 import FinalCountdown from '../../components/games/final-countdown-game';
+import { selectSlot, consumeSlots } from '../../actions/slots';
 
 export default connect((state) => ({
+    slots: state.slots.ids
 }), {
-
+    selectSlot,
+    consumeSlots
 }
 )(FinalCountdown);
