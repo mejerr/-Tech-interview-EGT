198239b97cd6cb0cb299914a0e00f64b03948af4
mejerr <georgy_90@abv.bg>
2021-09-06 00:23:23 +0300

    Loader added and loading state on timer

---
+++ casino-game/src/components/common/loader.js
@@ -0,0 +1,22 @@
+import React, { memo } from 'react';
+import classNames from 'classnames';
+import LoaderSpinner from "react-loader-spinner";
+
+const Loader = memo((props) => {
+    const { className } = props;
+
+    return (
+        <div className={classNames('loader-container', {
+            [className] : className
+            })}
+        >
+            <LoaderSpinner className="loader-state" width={30} height={30} type="Puff" color="#FFFFFF"/>
+        </div>
+    );
+});
+
+Loader.defaultProps = {
+    className: ''
+};
+
+export default Loader;
--- casino-game/src/components/common/timer.js
+++ casino-game/src/components/common/timer.js
@@ -3,6 +3,7 @@
 import React, { memo } from 'react'
 import { useState, useEffect } from 'react';
 import { timerCounter } from '../../utils/timer';
+import Loader from '../common/loader';
 
 const Timer = memo((props) => {
     const { onFinish } = props;
@@ -20,8 +21,8 @@ const Timer = memo((props) => {
     return (
         <div className="timer-container">
         {minutes === 0 && seconds === 0
-            ? <>Loader</>
-            : <div className="time">{minutes}:{seconds < 10 ?  `0${seconds}` : seconds}</div>
+            ? <Loader/>
+            : <div className={"time"}>{minutes}:{seconds < 10 ?  `0${seconds}` : seconds}</div>
         }
         </div>
     )
